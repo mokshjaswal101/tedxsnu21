7b64bd6340daa6504b2acf4475aecf2be7f1dbef
Pratham Aggarwal <prathu.agg@gmail.com>
2021-10-25 12:26:54 +0530

    feat(speakers): added a few changed to style function

--- src/pages/Home/Speakers.js
+++ src/pages/Home/Speakers.js
@@ -4,10 +4,18 @@ const svgs = reqSvgs.keys().map((path) => reqSvgs(path));
 
 function speakers() {
   const style = (index) => {
-    if (index === 0 || index === 8) return { marginLeft: "400px" };
-    else if (index === 7 || index === 1) return { marginLeft: "250px" };
-    else if (index === 6 || index === 2) return { marginLeft: "100px" };
-    else if (index === 5 || index === 3) return { marginLeft: "50px" };
+    const styleOb = {
+      marginLeft: "0px",
+      marginTop: "0px",
+      marginBottom: "0px",
+    };
+    if (index === 0 || index === 8) styleOb.marginLeft = "400px";
+    else if (index === 7 || index === 1) styleOb.marginLeft = "250px";
+    else if (index === 6 || index === 2) styleOb.marginLeft = "100px";
+    else if (index === 5 || index === 3) styleOb.marginLeft = "50px";
+    if (index > 4) styleOb.marginBottom = "-50px";
+    else if (index < 4) styleOb.marginTop = "-50px";
+    return styleOb;
   };
 
   return (
